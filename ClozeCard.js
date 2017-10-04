ca4166639089ed9b6ae0775bc44ce5f08be3156b
Tait Takatani <tait.takatani@gmail.com>
2017-10-03 18:17:37 -0700

    updated cloze

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,20 +1,22 @@
 function ClozeCard (text, cloze) {
   if (this instanceof ClozeCard) {
     this.fullText = text;
-    this.cloze = cloze;
-    this.partial = '';
-    if (text.toUpperCase().indexOf(cloze.toUpperCase()) !== -1) {
+    this.cloze = cloze !== undefined ? cloze.trim() : '';
+    if (this.cloze !== '' &&
+        text.toUpperCase().indexOf(cloze.toUpperCase()) !== -1) {
       this.partial = text.replace(new RegExp(cloze, 'ig'), '...');
+    } else {
+      this.logError();
     }
-    this.logError();
   } else {
     return new ClozeCard(text, cloze);
   }
 }
 
 ClozeCard.prototype.logError = function () {
-  if (this.partial === '') {
-    console.log(`Error: '${this.cloze}' is not in '${this.fullText}'`);
+  if (this.partial === undefined) {
+    if (this.cloze === '') console.log('Cloze cannot be empty');
+    else console.log(`Error: '${this.cloze}' is not in '${this.fullText}'`);
   }
 };
 
